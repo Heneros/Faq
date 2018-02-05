2f06a64251fb8df138ab1d3618a48d54bf3d4174
Rustam4457 <punk60826@gmail.com>
2018-02-05 20:59:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Faq on Angular
+
+## Develpment tools
+Bootstrap 4, Jquery
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListComponent } from './components/list/list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

4b719c1fe9ef8fff5225e47875fe6eb42db38932
Rustam4457 <punk60826@gmail.com>
2018-02-05 23:02:23 +0200

    Added question-list and models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ListComponent } from './components/list/list.component';
-
+import { QuestionListComponent } from './components/question-list/question-list.component';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ListComponent
+    QuestionListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
